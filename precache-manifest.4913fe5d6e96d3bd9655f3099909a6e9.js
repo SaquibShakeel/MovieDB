65f53590e39c5bf76956bad1a875106076946bf0
Md Saquib Shakeel <saquib.shakeel.1866@gmail.com>
2022-08-11 14:53:36 +0530

    Updates

---
+++ precache-manifest.4913fe5d6e96d3bd9655f3099909a6e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de86d4519e3eb4665312be36675c85b6",
+    "url": "/MovieDB/index.html"
+  },
+  {
+    "revision": "a8f90edcbdd5528a5ffd",
+    "url": "/MovieDB/static/css/main.fd16af2a.chunk.css"
+  },
+  {
+    "revision": "639bc3b12da8044f9fc5",
+    "url": "/MovieDB/static/js/2.b6f0f6d1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/MovieDB/static/js/2.b6f0f6d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f90edcbdd5528a5ffd",
+    "url": "/MovieDB/static/js/main.4b511af8.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/MovieDB/static/js/main.4b511af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "779c8e2e5a722626b74b",
+    "url": "/MovieDB/static/js/runtime-main.225ee73e.js"
+  }
+]);
